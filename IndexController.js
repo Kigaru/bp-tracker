2693371a00bb788e4959b9933ceef048335daba9
Kigaru <jakubster9@gmail.com>
2020-09-09 19:03:06 +0100

    now there is nothing in the html, everything is done through code muahaha

--- IndexController.js
+++ IndexController.js
@@ -12,21 +12,29 @@ function AnimateStart() {
     }
 }
 
-function BPSelect(input)
-{
+function BPSelect(input) {
     switch (input) {
         case "valorant":
             document.getElementById("bp-select-container").remove();
             break;
-    
+
         default:
             break;
     }
 }
 
-function CreateIntro()
-{
-
+function CreateIntro() {
+    var mainbody = document.getElementById("main-body");
+    mainbody.innerHTML =
+        `
+    <div id="bp-select-container">
+            <div id="bp-select" onclick='BPSelect("valorant")'>
+                <img src="assets/valologo.png" alt="valorant logo" id="bp-logo">
+                <span class="circle"></span>
+            </div>
+        </div>
+    `;
+    AnimateStart();
 }
 
-window.onload = AnimateStart;
+window.onload = CreateIntro;
